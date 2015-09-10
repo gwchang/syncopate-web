9e806a7b1c793f8ece62d859582fc4dc88480c5d
Gary Chang <gwh.chang@gmail.com>
2015-09-10 17:14:54 -0500

    function based views more explicit

--- syncopateweb/clusters/urls.py
+++ syncopateweb/clusters/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ClusterList.as_view(), name='cluster-list'),
-    url(r'^(?P<pk>[0-9]+)/$', views.ClusterDetail.as_view(), name='cluster-detail'),
+    url(r'^$', views.cluster_list, name='cluster-list'),
+    url(r'^(?P<pk>[0-9]+)/$', views.cluster_detail, name='cluster-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- syncopateweb/clusters/views.py
+++ syncopateweb/clusters/views.py
@@ -3,12 +3,48 @@
 
 from .models import Cluster
 from .serializers import ClusterSerializer
-from rest_framework import generics
+#from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-class ClusterList(generics.ListCreateAPIView):
-    queryset = Cluster.objects.all()
-    serializer_class = ClusterSerializer
+@api_view(['GET', 'POST'])
+def cluster_list(request, format=None):
+    """
+    List all clusters, or create a new cluster.
+    """
+    if request.method == 'GET':
+        clusters = Cluster.objects.all()
+        serializer = ClusterSerializer(clusters, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ClusterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class ClusterDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Cluster.objects.all()
-    serializer_class = ClusterSerializer
+@api_view(['GET', 'PUT', 'DELETE'])
+def cluster_detail(request, pk, format=None):
+    """
+    Retrieve, update or delete a cluster instance.
+    """
+    try:
+        cluster = Cluster.objects.get(pk=pk)
+    except Cluster.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ClusterSerializer(cluster)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ClusterSerializer(cluster, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        cluster.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
