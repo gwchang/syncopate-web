743af2145715cfdba7d4c181a807533db026bacc
Gary Chang <gwh.chang@gmail.com>
2015-09-10 23:33:36 -0500

    owner in serialized

--- syncopateweb/clusters/serializers.py
+++ syncopateweb/clusters/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Cluster
 
 class ClusterSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
     class Meta:
         model = Cluster
-        fields = ( 'name', 'api_key', 'token' )
+        fields = ( 'name', 'api_key', 'token', 'owner' )
