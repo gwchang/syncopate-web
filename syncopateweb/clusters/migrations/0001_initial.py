c33a12b70206fda33f20f308b5f14352bf30c697
Gary Chang <gwh.chang@gmail.com>
2015-09-10 21:11:47 -0500

    owner to Cluster model

--- syncopateweb/clusters/migrations/0001_initial.py
+++ syncopateweb/clusters/migrations/0001_initial.py
@@ -2,18 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Channel',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
                 ('group', models.CharField(max_length=200)),
                 ('topic', models.CharField(max_length=200)),
                 ('series_id', models.CharField(max_length=200)),
@@ -22,10 +24,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Cluster',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
                 ('name', models.CharField(max_length=200)),
                 ('api_key', models.CharField(max_length=200)),
                 ('token', models.CharField(max_length=200)),
+                ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.AddField(
--- syncopateweb/clusters/models.py
+++ syncopateweb/clusters/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Cluster(models.Model):
     name    = models.CharField(max_length=200)  # Human readable name
     api_key = models.CharField(max_length=200)  # Publish/upload key
     token   = models.CharField(max_length=200)  # Subscription key
+    owner   = models.ForeignKey(User)
 
 class Channel(models.Model):
     cluster     = models.ForeignKey(Cluster)        # Cluster than channel belongs to
